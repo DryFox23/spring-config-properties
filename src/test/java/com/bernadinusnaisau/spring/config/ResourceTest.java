209021e7e155f37d9b64e6b5f9c9bfbf4d402e58
bernad <bernadinusnaisau230803@gmail.com>
2025-08-22 18:29:20 +0700

    Belajar Spring Config: Implement Resource untuk mengambil/mengaksess sebuah file

---
+++ src/test/java/com/bernadinusnaisau/spring/config/ResourceTest.java
@@ -0,0 +1,19 @@
+package com.bernadinusnaisau.spring.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class ResourceTest {
+
+    @Test
+    void testGetResource() throws IOException {
+        var resource = new ClassPathResource("/text/sample.txt");
+
+        Assertions.assertNotNull(resource);
+        Assertions.assertTrue(resource.exists());
+        Assertions.assertNotNull(resource.getFile());
+    }
+}
