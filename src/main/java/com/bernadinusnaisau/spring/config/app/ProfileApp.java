f2779250e281ab43432503fa33e71a9158f3c68a
bernad <bernadinusnaisau230803@gmail.com>
2025-08-26 19:24:16 +0700

    Belajar Spring Config: Implementasi penggunaan profile di environment

--- src/main/java/com/bernadinusnaisau/spring/config/app/ProfileApp.java
+++ src/main/java/com/bernadinusnaisau/spring/config/app/ProfileApp.java
@@ -1,7 +1,10 @@
 package com.bernadinusnaisau.spring.config.app;
 
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -27,6 +30,17 @@ public class ProfileApp {
         @Override
         public String say(String name) {
             return "hello " + name + " from production";
+        }
+    }
+
+    @Component
+    public static class envProfile implements EnvironmentAware {
+
+        @Setter
+        private Environment environment;
+
+        public String[] getActiveProfiles() {
+            return environment.getActiveProfiles();
         }
     }
 }
