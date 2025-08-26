283b58307dd0ec962837551145d6e8a32380c546
bernad <bernadinusnaisau230803@gmail.com>
2025-08-26 19:55:36 +0700

    Belajar Spring Config: Implementasi penggunaan profile menggunakan banyak properties.application

---
+++ src/main/java/com/bernadinusnaisau/spring/config/app/ProfilePropertiesApp.java
@@ -0,0 +1,25 @@
+package com.bernadinusnaisau.spring.config.app;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ProfilePropertiesApp {
+
+    @Getter
+    @Component
+    public static class GetProfileProperties {
+
+        @Value("${profile.default}")
+        private String defaultProfile;
+
+        @Value("${profile.production}")
+        private String productionProfile;
+
+        @Value("${profiles.test}")
+        private String testProfile;
+    }
+}
