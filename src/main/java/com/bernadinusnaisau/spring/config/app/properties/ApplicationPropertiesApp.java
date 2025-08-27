80d283fb8e5d1bcbc89a9e3d1e1d1abb912450c7
bernad <bernadinusnaisau230803@gmail.com>
2025-08-27 19:35:02 +0700

    Belajar Spring Config: Implementasi class ConversionService sebagai pendukung untuk melakukan konversi tipe data string ke date

--- src/main/java/com/bernadinusnaisau/spring/config/app/properties/ApplicationPropertiesApp.java
+++ src/main/java/com/bernadinusnaisau/spring/config/app/properties/ApplicationPropertiesApp.java
@@ -1,13 +1,25 @@
 package com.bernadinusnaisau.spring.config.app.properties;
 
+import com.bernadinusnaisau.spring.config.app.converter.StringToDateConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.convert.ConversionService;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         ApplicationProperties.class
 })
+@Import(StringToDateConverter.class)
 public class ApplicationPropertiesApp {
+
+    @Bean
+    public ConversionService conversionService(StringToDateConverter stringToDateConverter) {
+        ApplicationConversionService conversionService = new ApplicationConversionService();
+        conversionService.addConverter(new StringToDateConverter());
+        return conversionService;
+    }
 
 }
